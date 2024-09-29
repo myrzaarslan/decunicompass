e6f141d947f3200d12aa60db0b348b29bff37c02
myrzaarslan <arsssslan@mail.ru>
2024-09-29 19:11:10 +0500

    added subject parameter to all API stuff

--- unicompass_app/urls.py
+++ unicompass_app/urls.py
@@ -10,7 +10,10 @@ urlpatterns = [
     path('api/uni/kz', views.kz_universities_list, name='kz_universities_list'),
     path('api/uni/the/<int:nid>/', views.the_university_detail, name='the_university_detail'),
     path('api/uni/qs/<int:nid>/', views.qs_university_detail, name='qs_university_detail'),
+    path('api/uni/the/<str:subject>/', views.the_universities_by_subjects, name='the_universities_by_subject'),
+    path('api/uni/qs/<str:subject>/', views.qs_universities_by_subjects, name='qs_universities_by_subjects'),
     path('api/uni/the/', views.the_universities_list, name='the_universities_list'),
     path('api/uni/qs/', views.qs_universities_list, name='qs_universities_list'),
     path('api/uni/', views.all_university_details, name='all_university_details'),
+    path('api/uni/<str:subject>/', views.universities_by_subject, name='universities_by_subject'),
 ]
--- unicompass_app/views.py
+++ unicompass_app/views.py
@@ -124,6 +124,109 @@ def qs_universities_list(request):
 
     return JsonResponse(response)
 
+def qs_universities_by_subjects(request, subject='general'):
+    page = int(request.GET.get('page', 0))
+    items_per_page = int(request.GET.get('items_per_page', 10))
+
+    valid_subject_fields = {
+        # Arts & Humanities
+        'arts_humanities': 'qs_rank_arts_humanities',
+        'arts': 'qs_rank_arts',
+        'linguistics': 'qs_rank_linguistics',
+        'music': 'qs_rank_music',
+        'theology': 'qs_rank_theology',
+        'archaeology': 'qs_rank_archaeology',
+        'architecture': 'qs_rank_architecture',
+        'art_design': 'qs_rank_art_design',
+        'classics': 'qs_rank_classics',
+        'english': 'qs_rank_english',
+        'history': 'qs_rank_history',
+        'art_history': 'qs_rank_art_history',
+        'modern_languages': 'qs_rank_modern_languages',
+        'performing_arts': 'qs_rank_performing_arts',
+        'philosophy': 'qs_rank_philosophy',
+
+        # Engineering & Technology
+        'engineering_technology': 'qs_rank_eng_tech',
+        'chemical_engineering': 'qs_rank_chem_eng',
+        'civil_engineering': 'qs_rank_civil_eng',
+        'computer_science': 'qs_rank_comp_sci',
+        'data_science': 'qs_rank_data_sci',
+        'electrical_engineering': 'qs_rank_elec_eng',
+        'petroleum_engineering': 'qs_rank_pet_eng',
+        'mechanical_engineering': 'qs_rank_mech_eng',
+        'mining_engineering': 'qs_rank_mining_eng',
+
+        # Natural Sciences
+        'natural_sciences': 'qs_rank_nat_sci',
+        'chemistry': 'qs_rank_chemistry',
+        'earth_marine_sciences': 'qs_rank_earth_marine_sci',
+        'environmental_sciences': 'qs_rank_env_sci',
+        'geography': 'qs_rank_geography',
+        'geology': 'qs_rank_geology',
+        'geophysics': 'qs_rank_geophysics',
+        'materials_science': 'qs_rank_materials_sci',
+        'mathematics': 'qs_rank_math',
+        'physics_astronomy': 'qs_rank_physics_astronomy',
+
+        # Life Sciences & Medicine
+        'life_sciences_medicine': 'qs_rank_life_sci',
+        'agriculture': 'qs_rank_agriculture',
+        'anatomy': 'qs_rank_anatomy',
+        'biological_sciences': 'qs_rank_bio_sci',
+        'dentistry': 'qs_rank_dentistry',
+        'medicine': 'qs_rank_medicine',
+        'pharmacy': 'qs_rank_pharmacy',
+        'nursing': 'qs_rank_nursing',
+        'psychology': 'qs_rank_psychology',
+        'veterinary_science': 'qs_rank_vet_sci',
+
+        # Social Sciences & Management
+        'social_sciences_management': 'qs_rank_social_sci_management',
+        'accounting_finance': 'qs_rank_accounting_finance',
+        'anthropology': 'qs_rank_anthropology',
+        'business_management': 'qs_rank_business_management',
+        'communication_media': 'qs_rank_comm_media',
+        'development_studies': 'qs_rank_dev_studies',
+        'economics_econometrics': 'qs_rank_economics_econometrics',
+        'education': 'qs_rank_education',
+        'hospitality_leisure': 'qs_rank_hospitality_leisure',
+        'law': 'qs_rank_law',
+        'politics_international_studies': 'qs_rank_politics_international_studies',
+        'social_policy_admin': 'qs_rank_social_policy_admin',
+        'sociology': 'qs_rank_sociology',
+        'sports_related': 'qs_rank_sports_related',
+
+        # Additional Subjects (if needed)
+        # Add more fields as necessary
+    }
+
+
+    # Base queryset with non-empty titles
+    base_queryset = UniUni.objects.exclude(Q(qs_title__isnull=True) | Q(qs_title=''))
+
+    if subject in valid_subject_fields:
+        ranking_field = valid_subject_fields[subject]
+        universities = base_queryset.filter(**{ranking_field + '__isnull': False}).order_by(ranking_field)
+    else:
+        universities = base_queryset.order_by('qs_rank')
+
+    total_records = universities.count()
+    total_pages = (total_records + items_per_page - 1) // items_per_page
+    universities = universities[(page * items_per_page):(page * items_per_page + items_per_page)]
+
+    data = list(universities.values('qs_nid', 'qs_rank', 'qs_title', 'qs_overall_score', 'qs_city', 'qs_country'))
+
+    response = {
+        "current_page": page + 1,
+        "total_pages": total_pages,
+        "items_per_page": items_per_page,
+        "total_records": total_records,
+        "data": data
+    }
+
+    return JsonResponse(response)
+
 def the_universities_list(request):
     subject_name = request.GET.get('subject', 'general')
     page = int(request.GET.get('page', 0))
@@ -139,6 +242,48 @@ def the_universities_list(request):
 
     if subject_name in valid_subject_fields:
         universities = base_queryset.filter(**{subject_name + '__isnull': False}).order_by(subject_name)
+    else:
+        universities = base_queryset.order_by('the_rank')
+
+    total_records = universities.count()
+    total_pages = (total_records + items_per_page - 1) // items_per_page
+    universities = universities[(page * items_per_page):(page * items_per_page + items_per_page)]
+
+    data = list(universities.values('the_nid', 'the_rank', 'the_title', 'the_overall_score', 'the_location', 'the_subjects_offered'))
+
+    response = {
+        "current_page": page + 1,
+        "total_pages": total_pages,
+        "items_per_page": items_per_page,
+        "total_records": total_records,
+        "data": data
+    }
+
+    return JsonResponse(response)
+
+def the_universities_by_subjects(request, subject='general'):
+    page = int(request.GET.get('page', 0))
+    items_per_page = int(request.GET.get('items_per_page', 10))
+
+    valid_subject_fields = {
+        'arts': 'the_rank_arts',
+        'business': 'the_rank_bus',
+        'clinical': 'the_rank_clin',
+        'computer': 'the_rank_comp',
+        'education': 'the_rank_edu',
+        'engineering': 'the_rank_eng',
+        'law': 'the_rank_law',
+        'life_sciences': 'the_rank_life',
+        'physical_sciences': 'the_rank_phys',
+        'psychology': 'the_rank_psych'
+    }
+
+    # Base queryset with non-empty titles
+    base_queryset = UniUni.objects.exclude(Q(the_title__isnull=True) | Q(the_title=''))
+
+    if subject in valid_subject_fields:
+        ranking_field = valid_subject_fields[subject]
+        universities = base_queryset.filter(**{ranking_field + '__isnull': False}).order_by(ranking_field)
     else:
         universities = base_queryset.order_by('the_rank')
 
@@ -194,6 +339,135 @@ def all_university_details(request):
 
         return JsonResponse(response)
 
+def universities_by_subject(request, subject):
+    page = int(request.GET.get('page', 0))
+    items_per_page = int(request.GET.get('items_per_page', 10))
+
+    valid_subject_fields = {
+    # THE subject fields
+    'the_arts': 'the_rank_arts',
+    'the_business': 'the_rank_bus',
+    'the_clinical': 'the_rank_clin',
+    'the_computer': 'the_rank_comp',
+    'the_education': 'the_rank_edu',
+    'the_engineering': 'the_rank_eng',
+    'the_law': 'the_rank_law',
+    'the_life_sciences': 'the_rank_life',
+    'the_physical_sciences': 'the_rank_phys',
+    'the_psychology': 'the_rank_psych',
+    
+    # QS subject fields
+    'qs_arts_humanities': 'qs_rank_arts_humanities',
+    'qs_engineering_tech': 'qs_rank_eng_tech',
+    'qs_life_sciences_medicine': 'qs_rank_life_sci',
+    'qs_natural_sciences': 'qs_rank_nat_sci',
+    
+    # Arts & Humanities
+    'qs_arts': 'qs_rank_arts',
+    'qs_linguistics': 'qs_rank_linguistics',
+    'qs_music': 'qs_rank_music',
+    'qs_theology': 'qs_rank_theology',
+    'qs_archaeology': 'qs_rank_archaeology',
+    'qs_architecture': 'qs_rank_architecture',
+    'qs_art_design': 'qs_rank_art_design',
+    'qs_classics': 'qs_rank_classics',
+    'qs_english': 'qs_rank_english',
+    'qs_history': 'qs_rank_history',
+    'qs_art_history': 'qs_rank_art_history',
+    'qs_modern_languages': 'qs_rank_modern_languages',
+    'qs_performing_arts': 'qs_rank_performing_arts',
+    'qs_philosophy': 'qs_rank_philosophy',
+
+    # Engineering & Technology
+    'qs_chemical_engineering': 'qs_rank_chem_eng',
+    'qs_civil_engineering': 'qs_rank_civil_eng',
+    'qs_computer_science': 'qs_rank_comp_sci',
+    'qs_data_science': 'qs_rank_data_sci',
+    'qs_electrical_engineering': 'qs_rank_elec_eng',
+    'qs_petroleum_engineering': 'qs_rank_pet_eng',
+    'qs_mechanical_engineering': 'qs_rank_mech_eng',
+    'qs_mining_engineering': 'qs_rank_mining_eng',
+
+    # Natural Sciences
+    'qs_chemistry': 'qs_rank_chemistry',
+    'qs_earth_marine_sciences': 'qs_rank_earth_marine_sci',
+    'qs_environmental_sciences': 'qs_rank_env_sci',
+    'qs_geography': 'qs_rank_geography',
+    'qs_geology': 'qs_rank_geology',
+    'qs_geophysics': 'qs_rank_geophysics',
+    'qs_materials_science': 'qs_rank_materials_sci',
+    'qs_mathematics': 'qs_rank_math',
+    'qs_physics_astronomy': 'qs_rank_physics_astronomy',
+
+    # Life Sciences & Medicine
+    'qs_agriculture': 'qs_rank_agriculture',
+    'qs_anatomy': 'qs_rank_anatomy',
+    'qs_biological_sciences': 'qs_rank_bio_sci',
+    'qs_dentistry': 'qs_rank_dentistry',
+    'qs_medicine': 'qs_rank_medicine',
+    'qs_pharmacy': 'qs_rank_pharmacy',
+    'qs_nursing': 'qs_rank_nursing',
+    'qs_psychology': 'qs_rank_psychology',
+    'qs_veterinary_science': 'qs_rank_vet_sci',
+
+    # Social Sciences & Management
+    'qs_accounting_finance': 'qs_rank_accounting_finance',
+    'qs_anthropology': 'qs_rank_anthropology',
+    'qs_business_management': 'qs_rank_business_management',
+    'qs_communication_media': 'qs_rank_comm_media',
+    'qs_development_studies': 'qs_rank_dev_studies',
+    'qs_economics_econometrics': 'qs_rank_economics_econometrics',
+    'qs_education': 'qs_rank_education',
+    'qs_hospitality_leisure': 'qs_rank_hospitality_leisure',
+    'qs_law': 'qs_rank_law',
+    'qs_politics_international_studies': 'qs_rank_politics_international_studies',
+    'qs_social_policy_admin': 'qs_rank_social_policy_admin',
+    'qs_sociology': 'qs_rank_sociology',
+    'qs_sports_related': 'qs_rank_sports_related',
+    }
+
+    if subject not in valid_subject_fields:
+        return JsonResponse({"error": "Invalid subject"}, status=400)
+
+    ranking_field = valid_subject_fields[subject]
+    ranking_system = 'the' if subject.startswith('the_') else 'qs'
+
+    universities = UniUni.objects.filter(**{ranking_field + '__isnull': False}).order_by(ranking_field)
+
+    total_records = universities.count()
+    total_pages = (total_records + items_per_page - 1) // items_per_page
+    universities = universities[(page * items_per_page):(page * items_per_page + items_per_page)]
+
+    data = []
+    for uni in universities:
+        uni_data = {
+            'id': uni.id,
+            'title': uni.the_title if ranking_system == 'the' else uni.qs_title,
+            'rank': getattr(uni, ranking_field),
+            'overall_score': uni.the_overall_score if ranking_system == 'the' else uni.qs_overall_score,
+        }
+        if ranking_system == 'the':
+            uni_data.update({
+                'location': uni.the_location,
+                'subjects_offered': uni.the_subjects_offered,
+            })
+        else:
+            uni_data.update({
+                'city': uni.qs_city,
+                'country': uni.qs_country,
+            })
+        data.append(uni_data)
+
+    response = {
+        "current_page": page + 1,
+        "total_pages": total_pages,
+        "items_per_page": items_per_page,
+        "total_records": total_records,
+        "data": data
+    }
+
+    return JsonResponse(response)
+
 def kz_universities_list(request):
     page = int(request.GET.get('page', 0))
     items_per_page = int(request.GET.get('items_per_page', 10))
@@ -220,6 +494,7 @@ def kz_universities_list(request):
     }
 
     return JsonResponse(response)
+
 def university(request, nid):
     # Check if university exists in either QS or THE models by link_id or nid
 
